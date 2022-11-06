6a4fb96a6d38ece57d897ba0f86174ada483079c
Lahiru Rajapaksha <lahiruzone@gmail.com>
2022-11-06 23:04:48 +0530

    feat(dark_mode): implimented theme toggle button

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,21 @@
+import 'package:fizzbuzz_app/core/app/theme/prasantation/cubit/theme_cubit.dart';
 import 'package:fizzbuzz_app/features/fizzbuzz/presantation/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FizzBuzz App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) {
+        return MaterialApp(
+          title: 'FizzBuzz App',
+          theme: state.themeData,
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:fizzbuzz_app/app.dart';
+import 'package:fizzbuzz_app/core/app/theme/prasantation/cubit/theme_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:fizzbuzz_app/injection_container.dart' as di;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   di.init();
-  runApp(const MyApp());
+  runApp(BlocProvider(
+    create: (context) => ThemeCubit(),
+    child: const MyApp(),
+  ));
 }
