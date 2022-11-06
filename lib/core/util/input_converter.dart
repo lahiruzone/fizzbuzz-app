69ffc69cce942a75874afeaae77ae757acb18523
Lahiru Rajapaksha <lahiruzone@gmail.com>
2022-11-06 14:22:54 +0530

    feat(manual_fizzbuzz_limit): implimented input converter

---
+++ lib/core/util/input_converter.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:fizzbuzz_app/core/entities/app_error.dart';
+
+class InputConverter {
+  InputConverterResult stringtoUnsignedInteger(String value) {
+    try {
+      final integer = int.parse(value);
+      if (integer < 0) throw const FormatException();
+      return InputConverterResult(value: integer);
+    } on FormatException {
+      return const InputConverterResult(error: ApplicationError(message: 'Invalied input'));
+    }
+  }
+}
+
+class InputConverterResult extends Equatable {
+  final int? value;
+  final ApplicationError? error;
+
+  const InputConverterResult({this.value, this.error});
+
+  @override
+  List<Object?> get props => [value, error];
+}
