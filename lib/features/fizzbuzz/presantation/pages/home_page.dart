b2ab3044bec446c357fcfd070949cc5e5c3cd2f7
Lahiru Rajapaksha <lahiruzone@gmail.com>
2022-11-07 15:15:00 +0530

    fix(user_input_overflow): chnaged input buttomsheet to alert dailog

--- lib/features/fizzbuzz/presantation/pages/home_page.dart
+++ lib/features/fizzbuzz/presantation/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fizzbuzz_app/config.dart';
 import 'package:fizzbuzz_app/features/fizzbuzz/presantation/bloc/fizzbuzz_bloc.dart';
-import 'package:fizzbuzz_app/features/fizzbuzz/presantation/widgets/home_page_floating_action_button_widget.dart';
+import 'package:fizzbuzz_app/features/fizzbuzz/presantation/widgets/home_page_change_limit_floating_action_button_widget.dart';
 import 'package:fizzbuzz_app/features/fizzbuzz/presantation/widgets/home_page_fzzbuzz_limit_display_text_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,7 +18,7 @@ class HomePage extends StatelessWidget {
         title: const HomePageFizzbuzzLimitDisplayTextWidget(),
         actions: const [HomePageThemeToggleButtonWidget()],
       ),
-      floatingActionButton: const HomePageFloatingActionButtonWidget(),
+      floatingActionButton: const HomePageChangeLimitFloatingActionButtonWidget(),
       body: const HomePageBodyWidget(),
     );
   }
